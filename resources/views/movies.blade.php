29a6ee1d991ed3e066efcb256f630fd10dc36f0c
Milica <magiflowerchild@gmial.com>
2022-02-16 18:48:55 +0100

    created movie model and controller and added movies blade file

---
+++ resources/views/movies.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <style>
+        a {
+            text-decoration: none;
+            color: black;
+        }
+
+    </style>
+</head>
+
+<body>
+    <div>
+        @foreach ($movies as $movie)
+            <h1><a href="movies/{{ $movie->id }}">{{ $movie->title }}</a></h1>
+            <p>{{ $movie->storyline }}</p>
+            <br>
+        @endforeach
+    </div>
+</body>
+
+</html>
